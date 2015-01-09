9e5a315678f1ee5ae9bf75c7e589cbd027732826
julien michot <ju.michot@gmail.com>
2015-01-09 11:16:22 +0100

    Add methods for retrieve access token

--- lib/nikeplus_client.rb
+++ lib/nikeplus_client.rb
@@ -1,5 +1,20 @@
-require "nikeplus_client/version"
+require 'nikeplus_client/version'
+require 'excon'
+require 'json'
 
 module NikeplusClient
-  # Your code goes here...
+  def self.get_token
+    response = request_token
+    extract_token_from_json_response(response.data[:body])
+  end
+
+  def self.request_token
+      Excon.post('https://developer.nike.com/services/login',
+      :body => URI.encode_www_form(:username => 'ju.michot@gmail.com', :password => 'Ek]@nsR6u!nD2nG}:t~_'),
+      :headers => { "Content-Type" => "application/x-www-form-urlencoded" })
+  end
+
+  def self.extract_token_from_json_response(json)
+    JSON.parse(json)["access_token"]
+  end
 end
--- spec/nikeplus_client_spec.rb
+++ spec/nikeplus_client_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
 describe NikeplusClient do
+  it "retrieve token" do
+    VCR.use_cassette("token_retrieval") do
+      expect(NikeplusClient.get_token()).to eq "thetoken"
+    end
+  end
 end
