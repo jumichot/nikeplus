1d304452072ac69cba7c46e8c9e2ea08ab90af8d
julien michot <ju.michot@gmail.com>
2015-01-09 15:01:55 +0100

    Handle when login/password are wrong

--- lib/nikeplus_client.rb
+++ lib/nikeplus_client.rb
@@ -3,18 +3,31 @@ require 'excon'
 require 'json'
 
 module NikeplusClient
-  def self.get_token
-    response = request_token
-    extract_token_from_json_response(response.data[:body])
-  end
+  class Client
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
 
-  def self.request_token
+    def token
+      response = request_token
+      response = extract_json_from_response_body(response.data[:body])
+      fail WrongCredential if response.key?('error')
+      @token = response['access_token']
+    end
+
+    private
+
+    def request_token
       Excon.post('https://developer.nike.com/services/login',
-      :body => URI.encode_www_form(:username => 'ju.michot@gmail.com', :password => 'Ek]@nsR6u!nD2nG}:t~_'),
-      :headers => { "Content-Type" => "application/x-www-form-urlencoded" })
+                 body: URI.encode_www_form(username: @username, password: @password),
+                 headers: { 'Content-Type' => 'application/x-www-form-urlencoded' })
+    end
+
+    def extract_json_from_response_body(json)
+      JSON.parse(json)
+    end
   end
 
-  def self.extract_token_from_json_response(json)
-    JSON.parse(json)["access_token"]
-  end
+  class WrongCredential < Exception; end
 end
--- spec/nikeplus_client_spec.rb
+++ spec/nikeplus_client_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
-describe NikeplusClient do
-  it "retrieve token" do
-    VCR.use_cassette("token_retrieval") do
-      expect(NikeplusClient.get_token()).to eq "thetoken"
+describe NikeplusClient::Client do
+  context "with right credentials" do
+    it "retrieve token" do
+      VCR.use_cassette("succeed_token_retrieval") do
+        nike_client = NikeplusClient::Client.new("foo@bar.com", "foobar")
+        expect(nike_client.token()).to eq "thetoken"
+      end
+    end
+  end
+
+  context "with wrong credentials" do
+    it "raise an explicit exception" do
+      VCR.use_cassette("failed_token_retrieval") do
+        nike_client = NikeplusClient::Client.new("wrong@bar.com", "wrong")
+        expect{nike_client.token()}.to raise_error(NikeplusClient::WrongCredential)
+      end
     end
   end
 end
