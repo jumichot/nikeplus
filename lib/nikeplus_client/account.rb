605515bd0a04c18df02f3b14fa7d128e50c59c45
julien michot <ju.michot@gmail.com>
2015-01-10 09:59:09 +0100

    Extract method on Account class

--- lib/nikeplus_client/account.rb
+++ lib/nikeplus_client/account.rb
@@ -8,11 +8,19 @@ module NikeplusClient
     def token
       return @access_token if @access_token
       response = extract_hash_from_json_response_body(request_token)
-      fail WrongCredential if response.key?('error')
+      check_token_validy(response)
       @access_token = response['access_token']
     end
 
     private
+
+    def check_token_validy(response)
+      fail WrongCredential unless token_valid?(response)
+    end
+
+    def token_valid?(response)
+      !response.key?('error')
+    end
 
     def request_token
       post_request('https://developer.nike.com/services/login', username: @username, password: @password)
