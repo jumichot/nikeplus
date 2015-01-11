94eafa26c64537de72a993a4be8ac7f79de9e233
julien michot <ju.michot@gmail.com>
2015-01-11 15:14:27 +0100

    Tell vcr to let codeclimate have his coverage results

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,4 +8,5 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
