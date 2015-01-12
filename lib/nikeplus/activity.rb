0547edb1802faf6746564161094c6980671a5857
julien michot <ju.michot@gmail.com>
2015-01-12 22:38:36 +0100

    Add possibility to fetch activity detail

--- lib/nikeplus/activity.rb
+++ lib/nikeplus/activity.rb
@@ -1,6 +1,9 @@
 module NikePlus
   class Activity
     include Virtus.model
+    include HTTPUtils
+
+    API_URL = "https://api.nike.com/v1/me/sport/activities"
 
     attribute :activityId, Integer
     attribute :activityType, String
@@ -10,6 +13,18 @@ module NikePlus
     attribute :deviceType, String
     attribute :metricSummary, MetricSummary
     attribute :tags, Array[Tag]
+
+    def fetch(options = {})
+      response = get_request(build_url(options))
+      data = extract_hash_from_json_response_body(response)
+      self.attributes = data
+    end
+
+    private
+
+    def api_url
+      API_URL + "/#{activityId}"
+    end
   end
 end
 
---
+++ spec/nikeplus/activity_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe NikePlus::Activity do
+  let(:token) { "foobar" }
+
+  describe "#fetch" do
+    it "#fetch return a new Activity" do
+      VCR.use_cassette("go_activity") do
+        activity = NikePlus::Activity.new(activityId: "3198000000011524937810007382618856401351")
+        activity.fetch(access_token: "foobar")
+        expect(activity).to be_kind_of(NikePlus::Activity)
+        expect(activity.activityType).to eq "RUN"
+      end
+    end
+  end
+end
